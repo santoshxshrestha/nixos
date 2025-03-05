f1eab320844af169a6f3c7ccd4abe3b6ee79edf4
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-05 20:33:43 +0545

    some shell

---
+++ shells/web.nix
@@ -0,0 +1,20 @@
+
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { 
+    config =  
+
+      { allowUnfree = true; };
+    overlays = []; 
+  };
+in
+  pkgs.mkShellNoCC {
+    packages = with pkgs; [
+      docker
+    ];
+    GREETING = "Hello, Nix!";
+    shellHook = ''
+    echo "Welcome to the development environment!"
+    echo $GREETING 
+    '';
+  }

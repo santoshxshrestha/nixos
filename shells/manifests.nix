7ec42854df151196c1c2b0e50e46dddfc0d03f9d
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-06 22:26:01 +0545

    some changes

--- shells/manifests.nix
+++ shells/manifests.nix
@@ -1,19 +1,15 @@
-
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
   pkgs = import nixpkgs { 
-    config =  
-
-      { allowUnfree = true; };
+    config = {
+      allowUnfree = true; 
+    };
     overlays = []; 
   };
 in
   pkgs.mkShellNoCC {
     packages = with pkgs; [
       docker
-      libreoffice-qt6-fresh
-      firefox
-      vscode
     ];
     GREETING = "Hello, Santosh!";
     shellHook = ''

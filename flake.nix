f3edf61fdd6b6d6bee21c7fde57b8f83829a34d5
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-07 18:03:45 +0545

    some changes

--- flake.nix
+++ flake.nix
@@ -1,61 +1,55 @@
 {
   description = "Reproducible Dev Environment";
-
   inputs = {
-    # Pinning nixpkgs to a specific version via the GitHub URL
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-24.05";  # Using the stable version from GitHub
-      flake = true;  # Indicate that this is a flake-based input
+      url = "github:NixOS/nixpkgs/nixos-unstable";  # Changed to unstable
+      flake = true;
     };
-    
     neovim-config = {
-      # Make the repo is public for clone
       url = "https://github.com/SantoshShrestha11/dotfiles";
       flake = false;
     };
   };
-
-  outputs = { self, nixpkgs, neovim-config }: let
-    # Import nixpkgs directly from the GitHub URL input
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.nodejs_23
-        pkgs.neovim
-        pkgs.git
-        pkgs.curl
-        pkgs.wget
-      ];
-
-      shellHook = ''
-        echo "Setting up global npm packages..."
-
-        # forwarding the auth keys I don't want to push from this env so commenting it out
-        # export SSH_AUTH_SOCK=$SSH_AUTH_SOCK
-
-        export NODE_PATH="/usr/lib/node_modules"
-        export PATH="$NODE_PATH/.bin:$PATH"
-
-        mkdir -p "$NODE_PATH"
-
-        npm install -g --prefix=/usr/lib corepack@0.31.0 live-server@1.2.2 \
+  outputs = { self, nixpkgs, neovim-config }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_23  # Now using nodejs 23 from unstable
+          pkgs.neovim
+          pkgs.git
+          pkgs.curl
+          pkgs.wget
+        ];
+        shellHook = ''
+          echo "Setting up global npm packages..."
+          export NPM_CONFIG_PREFIX="$HOME/.npm-global"
+          mkdir -p "$NPM_CONFIG_PREFIX"
+          export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+          npm install -g corepack@0.31.0 live-server@1.2.2 \
           node-gyp@11.1.0 nopt@7.2.1 npm@11.1.0 semver@7.7.1 sql-language-server@1.7.1
-        echo "Cloning Neovim config ..."
+          echo "Inspecting neovim-config structure:"
+          ls -la ${neovim-config}
+  ls -la ${neovim-config}/nvim || echo "nvim directory not found"
 
-        echo "Linking Neovim config..."
+  mkdir -p $HOME/Nix/.config
+  rm -rf $HOME/Nix/.config/nvim
 
-        mkdir -p $HOME/nix-nvim
-        # deleting the existing config file to reduce the conflicts
-        rm -rf ~/nix-nvim/.config/nvim
-
-        # Creates the symbolic link to the Neovim config
-        ln -s ${neovim-config}/nvim/.config/nvim $HOME/nix-nvim/.config/nvim
-
-        export XDG_CONFIG_HOME="$HOME/nix-nvim/.config"
-        echo "Development environment ready!"
-      '';
+  # Try direct path if the structure is different
+  if [ -d "${neovim-config}/nvim/.config/nvim" ]; then
+  ln -s ${neovim-config}/nvim/.config/nvim $HOME/Nix/.config/nvim
+  elif [ -d "${neovim-config}/.config/nvim" ]; then
+  ln -s ${neovim-config}/.config/nvim $HOME/Nix/.config/nvim
+  elif [ -d "${neovim-config}/nvim" ]; then
+  ln -s ${neovim-config}/nvim $HOME/Nix/.config/nvim
+  else
+  echo "ERROR: Could not find neovim config in expected locations"
+  ls -R ${neovim-config}
+  fi          export XDG_CONFIG_HOME="$HOME/Nix/.config"
+  echo "Development environment ready!"
+  '';
+      };
     };
-  };
 }
-

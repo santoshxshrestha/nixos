906a2302e1d8dde7d4ed3bdd45d91844b865a30b
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-13 16:57:51 +0545

    Create packages.nix
    added the packages in the different directory

---
+++ packages.nix
@@ -0,0 +1,30 @@
+
+  {pkgs, ...} :
+  {
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    neovim
+    firefox
+    hyprland
+    starship
+    hyprpaper
+    kitty
+    tmux
+    zsh
+    stow
+    rofi-wayland
+    waybar
+    networkmanagerapplet
+    pavucontrol
+    wl-clipboard
+    brightnessctl
+    hyprlock
+    fzf
+    unzip
+    hyprcursor
+    rose-pine-hyprcursor
+
+  ];
+  }

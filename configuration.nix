072ff6910290c732f802dcc01234b8a2c15971db
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-13 18:43:01 +0545

    some more management of the packages

--- configuration.nix
+++ configuration.nix
@@ -47,18 +47,6 @@
   };
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  fonts.packages = with pkgs; [
-  noto-fonts
-  noto-fonts-cjk-sans
-  noto-fonts-emoji
-  liberation_ttf
-  fira-code
-  fira-code-symbols
-  mplus-outline-fonts.githubRelease
-  dina-font
-  proggyfonts
-];
 
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
--- packages.nix
+++ packages.nix
@@ -32,4 +32,17 @@
   hyprpaper
   ];
 
+  nixpkgs.config.allowUnfree = true;
+  fonts.packages = with pkgs; [
+  noto-fonts
+  noto-fonts-cjk-sans
+  noto-fonts-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+  ];
+
   }

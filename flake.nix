af25b89ac92e06dda93b1459cc603291c87fbbe9
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-13 23:05:50 +0545

    home manager

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
 	inputs = {
 		nixpkgs = {
 			url = "github:NixOS/nixpkgs/nixos-unstable";
-			home-manager.url = "github:nix-community/home-manager/master";
-			home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
 		};
+
+			home-manager.url = "github:nix-community/home-manager";
+			home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... }:

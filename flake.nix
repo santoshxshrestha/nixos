6ab8787a65d6485dee6668e86d6597ab321f54e0
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-13 22:40:58 +0545

    home manager

--- flake.nix
+++ flake.nix
@@ -1,20 +1,37 @@
 {
-  description = "flake for santosh";
+	description = "flake for santosh";
 
-  inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-  };
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+			home-manager.url = "github:nix-community/home-manager/master";
+			home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+		};
+	};
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      santosh = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
-      };
-    };
-  };
+	outputs = { self, nixpkgs, home-manager, ... }:
+		let 
+		lib = nixpkgs.lib ;
+		system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system}";
+	in{
+
+		nixosConfigurations = {
+			santosh = nixpkgs.lib.nixosSystem {
+			inherit system;
+				modules = [
+					./configuration.nix
+				];
+			};
+		};
+
+		homeConfigurations = {
+			santosh = home-manager.lib.homeManagerConfiguration{
+				inherit pkgs;
+				modules = [
+					./home.nix
+				];
+			};
+		};
+	};
 }

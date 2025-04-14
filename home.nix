87385154daafdbb85544016906448cf6a6f468e9
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-14 14:45:50 +0545

    added hyprsunset

--- home.nix
+++ home.nix
@@ -47,6 +47,7 @@
 		".config/kitty".source = ./kitty;
 		".tmux.conf".source = ./tmux/.tmux.conf;
 		".config/hypr/hyprlock.conf".source = ./hyprlock/hyprlock.conf;
+		".config/hypr/hyprsunset.conf".source = ./hyprsunset.conf;
 
 # # Building this configuration will create a copy of 'dotfiles/screenrc' in
 # # the Nix store. Activating the configuration will then make '~/.screenrc' a

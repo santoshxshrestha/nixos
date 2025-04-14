96748cf22f727d5827d06853300fe54bcf04c6cb
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-14 13:21:52 +0545

    imports removed

--- usr/home.nix
+++ usr/home.nix
@@ -2,9 +2,9 @@
 
 {
 
-	imports = [
-		./hyprland.nix
-	];
+	# imports = [
+		# ./hyprland.nix
+	# ];
 # Home Manager needs a bit of information about you and the paths it should
 # manage.
 	home.username = "santosh";

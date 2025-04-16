1cde08db3ed84bf3a01749b1f83bed74d8ea15c4
santoshxshrestha <santoshxshrestha@gmail.com>
2025-04-16 17:28:33 +0545

    hyprpanel

--- flake.nix
+++ flake.nix
@@ -9,13 +9,13 @@
 #		url = "github:hyprwm/hyprland-plugins";
 #		follows = "hyprland";
 #	};
-		zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    # zen-browser.url = "github:0xc000022070/zen-browser-flake";
 		nixpkgs = {
 			url = "github:NixOS/nixpkgs/nixos-unstable";
 		};
 
-		home-manager.url = "github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+			home-manager.url = "github:nix-community/home-manager";
+			home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
 	};
 
 	outputs = inputs@{ self, nixpkgs, home-manager, ... }:
@@ -27,17 +27,19 @@
 
 		nixosConfigurations = {
 			santosh = lib.nixosSystem {
+				inherit system;
 				modules = [
 					./configuration.nix
 				];
 			};
-			homeConfigurations = {
-				santosh = home-manager.lib.homeManagerConfiguration{
-					inherit pkgs;
-					modules = [
-						./home-manager/home.nix
-					];
-				};
+		};
+
+		homeConfigurations = {
+			santosh = home-manager.lib.homeManagerConfiguration{
+				inherit pkgs;
+				modules = [
+					./home-manager/home.nix
+				];
 			};
 		};
 	};

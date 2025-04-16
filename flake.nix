0e784da1c87a105a212a92ea6dbb2b5d385881c8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-04-16 17:15:06 +0545

    hyprpanel

--- flake.nix
+++ flake.nix
@@ -2,22 +2,23 @@
 	description = "flake for santosh";
 
 	inputs = {
+		hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
 #	hypland.url = "github:hyprwm/Hyprland";
 
 #	hyprland-plugins = {
 #		url = "github:hyprwm/hyprland-plugins";
 #		follows = "hyprland";
 #	};
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+		zen-browser.url = "github:0xc000022070/zen-browser-flake";
 		nixpkgs = {
 			url = "github:NixOS/nixpkgs/nixos-unstable";
 		};
 
-			home-manager.url = "github:nix-community/home-manager";
-			home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+		home-manager.url = "github:nix-community/home-manager";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
 	};
 
-	outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+	outputs = inputs@{ self, nixpkgs, home-manager, hyprpanel, ... }:
 		let 
 		lib = nixpkgs.lib ;
 	system = "x86_64-linux";
@@ -26,19 +27,23 @@
 
 		nixosConfigurations = {
 			santosh = lib.nixosSystem {
-				inherit system;
 				modules = [
 					./configuration.nix
 				];
 			};
-		};
+			homeConfigurations = {
+				santosh = home-manager.lib.homeManagerConfiguration{
+					inherit pkgs;
+					modules = [
+						./home-manager/home.nix
+					];
 
-		homeConfigurations = {
-			santosh = home-manager.lib.homeManagerConfiguration{
-				inherit pkgs;
-				modules = [
-					./home-manager/home.nix
-				];
+					programs.hyprpanel = {
+						enable = true;
+						overwrite.enable = true;
+						overlay.enable = true;
+					};
+				};
 			};
 		};
 	};

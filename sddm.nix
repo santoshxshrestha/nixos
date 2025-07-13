d2785ab9815af17357532dc0846a3f08fa864ef2
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-14 02:20:59 +0545

    sddm config

--- sddm.nix
+++ sddm.nix
@@ -4,15 +4,30 @@ let
     owner = "santoshxshrestha";
     repo = "sddm-greenleaf";
     rev = "d21828cc54d1e0c25d5d026539bcb13503ed07b9";
-    # sha256 = pkgs.lib.fakeSha256;  
     sha256 = "sha256-gB2ljFqhm4SaI5M/IfU37gySdIn7BQH7PlDRIv66Hpo=";
   };
 in
 {
   services.displayManager.sddm = {
     enable = true;
-    theme = "sddm-greenleaf";
+    theme = "greenleaf";
+    
+    package = pkgs.kdePackages.sddm;
+    
+    settings = {
+      Theme = {
+        Current = "greenleaf";
+        ThemeDir = "/etc/sddm/themes";
+      };
+    };
   };
   
-  environment.etc."sddm/themes/sddm-greenleaf".source = myCustomSddmTheme;
+  environment.etc."sddm/themes/greenleaf".source = myCustomSddmTheme;
+  
+  environment.systemPackages = with pkgs; [
+    kdePackages.qt5compat      # qt6-5compat
+    kdePackages.qtdeclarative  # qt6-declarative
+    kdePackages.qtsvg          # qt6-svg
+    
+  ];
 }

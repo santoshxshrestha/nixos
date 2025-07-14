a7cca294e44855ec53fecadb9725d3d9941b24fb
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-14 16:03:20 +0545

    latest config of the fish

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -23,32 +23,9 @@
       "asdf" = "~/.local/scripts/sessionizer";
       "rebuild" = "sudo nixos-rebuild switch --flake ~/nixos/#santosh";
     };
-    
-    # plugins = [
-      # {
-      #   name = "fzf-fish";
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "PatrickF1";
-      #     repo = "fzf.fish";
-      #     rev = "v10.3";
-      #     sha256 = "1hqqppna8iwjnm8135qdjbd093583qd2kbq8pj507zpb1wn9ihjg";
-      #   };
-      # }
-      # {
-      #   name = "tide";
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "IlanCosman";
-      #     repo = "tide";
-      #     rev = "v6.0.1";
-      #     sha256 = "sha256-oLD7gYFCIeIzBeAW1j62z4nQhJhGhzm8J3hLEqK8+w4=";
-      #   };
-      # }
-    # ];
   };
 
   programs.fzf = {
     enable = true;
-    # keybindings = true;
-    # fuzzyCompletion = true;
   };
 }

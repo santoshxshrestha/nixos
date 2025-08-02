edae6d87978373b7e75bec8bc2be7ded8fcf62b7
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-03 02:14:14 +0545

    better config of thigne

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -1,67 +1,104 @@
 { config, ... }: {
-  programs.nixvim = {
+	programs.nixvim = {
+		enable = true;
+
+		colorschemes.catppuccin.enable = true;
+		globals.mapleader = " ";
+
+		globals.maplocalleader = " ";
+
+		plugins = {
+			lualine.enable = true;
+			lspconfig.enable = true; 
+			telescope= {
+				enable = true;
+
+				keymaps = {
+					"<leader>sf" = "find_files";
+					"<leader>sg" = "live_grep";
+					"<leader><leader>" = "buffers";
+					"<leader>ss" = "help_tags";
+				};
+				extensions = {
+					fzf-native = {
+						enable = true;
+						settings = {
+							fuzzy = true;
+							override_generic_sorter = true;
+							override_file_sorter = true;
+							case_mode = "smart_case";
+						};
+					};
+				};
+			};
+			web-devicons.enable = true;
+null-ls = {
     enable = true;
-
-    colorschemes.catppuccin.enable = true;
-
-    plugins = {
-      lualine.enable = true;
-
-      lspconfig = { enable = true; };
-
+    sources = {
+      formatting.nixfmt.enable = true;
+      # You can add other formatters here
+      formatting.stylua.enable = true;
     };
+  };		};
 
-    lsp.servers = {
+		lsp.servers = {
 
-      "*" = {
-        settings = {
-          capabilities = {
-            textDocument = {
-              semanticTokens = { multilineTokenSupport = true; };
-            };
-          };
-          root_markers = [ ".git" ];
-        };
-      };
+			"*" = {
+				settings = {
+					capabilities = {
+						textDocument = {
+							semanticTokens = { multilineTokenSupport = true; };
+						};
+					};
+					root_markers = [ ".git" ];
+				};
+			};
+			nixd = {
+				enable = true;
+				settings = {
+# Optional: configure nixd settings
+					formatting = {
+						command = [ "nixfmt" ];
+					};
+				};
+			};
 
-      clangd = {
-        enable = true;
-        settings = { filetypes = [ "c" "cpp" ]; };
-      };
+			clangd = {
+				enable = true;
+				settings = { filetypes = [ "c" "cpp" ]; };
+			};
 
-      lua_ls.enable = true;
-    };
-    # lsp.servers.clangd.enable = true;
-    # lsp.servers.lua_ls.enable = true;
-    lsp.inlayHints.enable = true;
-    lsp.keymaps =
+			lua_ls.enable = true;
+		};
+		lsp.inlayHints.enable = true;
+		lsp.keymaps =
 
-      [
-        {
-          key = "gd";
-          lspBufAction = "definition";
-        }
-        {
-          key = "gD";
-          lspBufAction = "references";
-        }
-        {
-          key = "gt";
-          lspBufAction = "type_definition";
-        }
-        {
-          key = "gi";
-          lspBufAction = "implementation";
-        }
-        {
-          key = "K";
-          lspBufAction = "hover";
-        }
-        {
-          action = "<CMD>Lspsaga hover_doc<Enter>";
-          key = "K";
-        }
-      ];
+			[
+			{
+				key = "gd";
+				lspBufAction = "definition";
+			}
+		{
+			key = "gD";
+			lspBufAction = "references";
+		}
+		{
+			key = "gt";
+			lspBufAction = "type_definition";
+		}
+		{
+			key = "gi";
+			lspBufAction = "implementation";
+		}
+		{
+			key = "K";
+			lspBufAction = "hover";
+		}
+		{
+			action = "<CMD>Lspsaga hover_doc<Enter>";
+			key = "K";
+		}
+		];
 
-  };
-}
+	};
+		 }

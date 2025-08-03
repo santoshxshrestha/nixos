078357be2fdcfacce505a82d7ce217e3fc576883
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-03 16:04:02 +0545

    config

--- home-manager/modules/nixvim/core/keymaps.nix
+++ home-manager/modules/nixvim/core/keymaps.nix
@@ -1,8 +1,26 @@
-{ config }: {
-  keymaps = {
-    "<leader>sf" = "find_files";
-    "<leader>sg" = "live_grep";
-    "<leader><leader>" = "buffers";
-    "<leader>ss" = "help_tags";
-  };
+programs.nixvim.keymaps = [
+{
+	action = "find_filies";
+	key = "<leader>sf";
+	mode = [ "n" ] ;
+	options = { desc = "Search files"; };
 }
+{
+	action = "live_grep";
+	key = "<leader>sg;
+	mode = [ "n" ] ;
+	options = { desc = "Search using live grep"; };
+}
+{
+	key = "<leader><leader>";
+	action = "buffers";
+	mode = [ "n" ] ;
+	options = { desc = "List buffers"; };
+}
+{
+	key = "<leader>ss";
+	action = "help
+		mode = [ "n" ] ;
+	options = { desc = " help tags";};
+}
+];

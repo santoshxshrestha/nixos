618d225993580d8198658ae600917b38bd274253
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-04 16:18:15 +0545

    enabled

--- home-manager/modules/nixvim/plugins/indent-blankline.nix
+++ home-manager/modules/nixvim/plugins/indent-blankline.nix
@@ -1,17 +1,21 @@
 {
-  programs.nixvim.plugins.indent-blankline.settings = {
-    indent = {
-      char = "│";
-      tab_char = "│"; # Character for tab indentation
-    };
-    whitespace = {
-      highlight = [ "Whitespace" "NonText" ]; # Highlight groups for whitespace
-    };
-    scope = {
-      show_start = false;
-      show_end = false;
-      show_exact_scope = false;
-      highlight = [ "Function" "Label" ]; # Highlight groups for scope
+  programs.nixvim.plugins.indent-blankline = {
+    enable = true;
+    settings = {
+      indent = {
+        char = "│";
+        tab_char = "│"; # Character for tab indentation
+      };
+      whitespace = {
+        highlight =
+          [ "Whitespace" "NonText" ]; # Highlight groups for whitespace
+      };
+      scope = {
+        show_start = false;
+        show_end = false;
+        show_exact_scope = false;
+        highlight = [ "Function" "Label" ]; # Highlight groups for scope
+      };
     };
   };
 }

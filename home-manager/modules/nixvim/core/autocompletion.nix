70c894ca94653c3e23dc996fa3bcf28c05fca83f
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-04 23:52:14 +0545

    update the key binding

--- home-manager/modules/nixvim/core/autocompletion.nix
+++ home-manager/modules/nixvim/core/autocompletion.nix
@@ -4,15 +4,12 @@
       enable = true;
       autoEnableSources = true;
       settings = {
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
+        sources =
+          [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
         mapping = {
-            "<C-n>" = "cmp.mapping.select_next_item()";
-            "<C-p>" = "cmp.mapping.select_prev_item()";        
-            "<C-y>" = "cmp.mapping.confirm({ select = true })"; 
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-y>" = "cmp.mapping.confirm({ select = true })";
 
           "<C-Space>" = "cmp.mapping.complete()";
 
@@ -20,15 +17,16 @@
           "<C-e>" = "cmp.mapping.close()";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
 
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-         # "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-         # "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          # here this one is for response of pressing the enter
+          # "<CR>" = "cmp.mapping.confirm({ select = true })";
+          # "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          # "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
       };
     };
-    
-    cmp-nvim-lsp.enable = true; 
-    cmp-path.enable = true;    
-    cmp-buffer.enable = true; 
+
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true;
+    cmp-buffer.enable = true;
   };
 }

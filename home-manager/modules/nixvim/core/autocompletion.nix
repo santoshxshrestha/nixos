2678c545b1b29c5a6a315654a1f188ba5e46981f
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-05 17:23:48 +0545

    finally done wiht it

--- home-manager/modules/nixvim/core/autocompletion.nix
+++ home-manager/modules/nixvim/core/autocompletion.nix
@@ -4,8 +4,16 @@
       enable = true;
       autoEnableSources = true;
       settings = {
-        sources =
-          [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "buffer"; }
+          { name = "luasnip"; }
+        ];
+        snippet = {
+          expand =
+            "function(args) require('luasnip').lsp_expand(args.body) end";
+        };
         window = {
           documentation = { border = [ "┌" "─" "┐" "│" "┘" "─" "└" "│" ]; };
           winhighlight =
@@ -18,6 +26,10 @@
         };
 
         mapping = {
+          "<C-l>" =
+            "cmp.mapping(function(fallback) local luasnip = require('luasnip') if luasnip.expand_or_locally_jumpable() then luasnip.expand_or_jump() else fallback() end end, {'i', 's'})";
+          "<C-h>" =
+            "cmp.mapping(function(fallback) local luasnip = require('luasnip') if luasnip.locally_jumpable(-1) then luasnip.jump(-1) else fallback() end end, {'i', 's'})";
           "<C-n>" = "cmp.mapping.select_next_item()";
           "<C-p>" = "cmp.mapping.select_prev_item()";
           "<C-y>" = "cmp.mapping.confirm({ select = true })";
@@ -39,5 +51,7 @@
     cmp-nvim-lsp.enable = true;
     cmp-path.enable = true;
     cmp-buffer.enable = true;
+    cmp_luasnip.enable = true;
+    luasnip.enable = true;
   };
 }

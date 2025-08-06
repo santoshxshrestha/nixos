a230e627878cfa0809353240e703f24275856c6e
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-06 16:51:28 +0545

    update config of the color theme

--- configuration.nix
+++ configuration.nix
@@ -5,143 +5,143 @@
 { config, pkgs, inputs, ... }:
 
 {
-    imports = [ # Include the results of the hardware scan.
-        ./hardware-configuration.nix
-        ./modules/packages.nix
-        ./modules/sddm.nix
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./modules/packages.nix
+    ./modules/sddm.nix
 
-    ];
+  ];
 
-# Bootloader.
-    boot.loader = {
-        systemd-boot = {
-            enable = true;
-            configurationLimit = 20;
-            consoleMode = "auto";
+  # Bootloader.
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 20;
+      consoleMode = "auto";
+    };
+
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    timeout = 5;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kathmandu";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.santosh = {
+    isNormalUser = true;
+    description = "Santosh Shrestha";
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+    shell = pkgs.fish;
+  };
+
+  programs.fish.enable = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  services.xserver.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    # package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    xwayland.enable = true;
+  };
+
+  hardware.graphics.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  services.dbus.enable = true;
+
+  security.polkit.enable = true;
+
+  # remap 
+  services.keyd = {
+    enable = true;
+    keyboards.default = {
+      ids = [ "*" ];
+      settings = {
+        main = { capslock = "overload(control, esc)"; };
+        # Optional: adjust timing (default is usually fine)
+        global = {
+          overload_tap_timeout = "200"; # milliseconds
         };
-
-        efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot";
-        };
-
-        timeout = 5;
+      };
     };
+  };
 
-    networking.hostName = "nixos"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # https://nix.dev/guides/faq#how-to-run-non-nix-executables
+  programs.command-not-found.enable = true;
+  programs.nix-ld.enable = true;
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  programs.nix-ld.libraries = with pkgs; [ clang-tools ];
 
-# Enable networking
-        networking.networkmanager.enable = true;
+  #i18n.inputMethod.enable = "fcitx5";
+  #i18n.inputMethod.fcitx5.addons= with pkgs;
+  #[fcitx5-mozc];
 
-# Set your time zone.
-    time.timeZone = "Asia/Kathmandu";
+  #services.logind = {
+  #enable = true;
+  #lidSwitch = "ignore";
+  #lidSwitchDocked = "ignore";
+  #};
 
-# Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-# Configure keymap in X11
-    services.xserver.xkb = {
-        layout = "us";
-        variant = "";
-    };
+  # List services that you want to enable:
 
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-# Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.santosh = {
-        isNormalUser = true;
-        description = "Santosh Shrestha";
-        extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
-        shell = pkgs.fish;
-    };
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-    programs.fish.enable = true;
-# Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-    services.xserver.enable = true;
-
-    programs.hyprland = {
-        enable = true;
-        withUWSM = true;
-# package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-        xwayland.enable = true;
-    };
-
-    hardware.graphics.enable = true;
-
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        pulse.enable = true;
-    };
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-    services.dbus.enable = true;
-
-    security.polkit.enable = true;
-
-# remap 
-    services.keyd = {
-        enable = true;
-        keyboards.default = {
-            ids = [ "*" ];
-            settings = {
-                main = { capslock = "overload(control, esc)"; };
-# Optional: adjust timing (default is usually fine)
-                global = {
-                    overload_tap_timeout = "200"; # milliseconds
-                };
-            };
-        };
-    };
-
-# https://nix.dev/guides/faq#how-to-run-non-nix-executables
-    programs.command-not-found.enable = true;
-    programs.nix-ld.enable = true;
-
-    programs.nix-ld.libraries = with pkgs; [ clang-tools ];
-
-#i18n.inputMethod.enable = "fcitx5";
-#i18n.inputMethod.fcitx5.addons= with pkgs;
-#[fcitx5-mozc];
-
-#services.logind = {
-#enable = true;
-#lidSwitch = "ignore";
-#lidSwitchDocked = "ignore";
-#};
-
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
-
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
-
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.11"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
--- home-manager/modules/nixvim/plugins/colorschemes.nix
+++ home-manager/modules/nixvim/plugins/colorschemes.nix
@@ -1,9 +1,62 @@
-{
-  programs.nixvim.colorschemes.catppuccin = {
-    enable = true;
-    settings = {
-      show_end_of_buffer = false;
-      transparent_background = false;
+{ pkgs, ... }: {
+  programs.nixvim = {
+    colorschemes = {
+      catppuccin = {
+        enable = false;
+        settings = {
+          show_end_of_buffer = false;
+          transparent_background = false;
+          flavor = "mocha";
+        };
+      };
+
+      rose-pine = {
+        enable = true;
+        settings = {
+          show_end_of_buffer = false;
+          flavor = "main";
+          disable_background = true;
+          disable_float_background = true;
+        };
+      };
     };
+
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "vague-nvim";
+        version = "2024-01-01";
+        src = pkgs.fetchFromGitHub {
+          owner = "vague2k";
+          repo = "vague.nvim";
+          rev = "main";
+          sha256 = "sha256-rJe0pJu6/JKCCdhKTwPFdHlZ2CUaIJc++nlsEdYXQOY=";
+        };
+      })
+    ];
+
+    extraConfigLua = ''
+      require('vague').setup({
+        transparent = true,
+        -- style = {
+        --   boolean = "italic",
+        --   number = "none",
+        --   float = "none",
+        --   error = "none",
+        --   comments = "italic",
+        --   conditionals = "none",
+        --   functions = "none",
+        --   headings = "bold",
+        --   operators = "none",
+        --   strings = "none",
+        --   variables = "none",
+        --   keywords = "none",
+        --   keyword_return = "none",
+        --   keywords_loop = "none",
+        --   keywords_label = "none",
+        --   keywords_exception = "none",
+        -- },
+      })
+      -- vim.cmd('colorscheme vague')
+    '';
   };
 }

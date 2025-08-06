4d1b308a75c214e51a25cab2204ccab1aba1dfb9
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-06 10:47:47 +0545

    update the color scheme

--- home-manager/modules/nixvim/plugins/colorschemes.nix
+++ home-manager/modules/nixvim/plugins/colorschemes.nix
@@ -1,1 +1,9 @@
-{ programs.nixvim.colorschemes.catppuccin = { enable = true; }; }
+{
+  programs.nixvim.colorschemes.catppuccin = {
+    enable = true;
+    settings = {
+      show_end_of_buffer = false;
+      transparent_background = false;
+    };
+  };
+}

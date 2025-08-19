4f88a944cea651c852bbfc026f7e48af7a8bf1e3
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-19 21:07:31 +0545

    update stylix config

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,6 +1,29 @@
 { pkgs, ... }: {
   stylix = {
     enable = true;
+    image = ../wallpapers/anime-night-sky-illustration.jpg;
+
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+    # base16Scheme = {
+    #   base00 = "#1c1c1c";
+    #   base01 = "#2a2a2a";
+    #   base02 = "#3a3a3a";
+    #   base03 = "#505050";
+    #   base04 = "#b0b0b0";
+    #   base05 = "#d0d0d0";
+    #   base06 = "#e0e0e0";
+    #   base07 = "#ffffff";
+    #   base08 = "#fb4934";
+    #   base09 = "#fe8019";
+    #   base0A = "#fabd2f";
+    #   base0B = "#8ec07c";
+    #   base0C = "#8ec0c0";
+    #   base0D = "#83a598";
+    #   base0E = "#d3869b";
+    #   base0F = "#d65d0e";
+    # };
 
     fonts = {
       serif = {
@@ -22,15 +45,14 @@
         name = "Noto Emoji";
         package = pkgs.noto-fonts-monochrome-emoji;
       };
+
       sizes = {
         applications = 10;
         terminal = 10;
         desktop = 10;
         popups = 10;
       };
-    };
 
-    image = ../wallpapers/nix-dark.png;
-    polarity = "dark";
+    };
   };
 }

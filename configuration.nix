b9d1f0462e31a78f6d56969160b5cecfffdf2949
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-19 18:03:09 +0545

    update config of modularitiy

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./modules/sddm/sddm.nix
-    ./pkgs/system/packages.nix
+    ./modules/packages.nix
 
   ];
 

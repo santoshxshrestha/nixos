6f9465c17eb47e23bb9d8d08fc85ed4b45e5318d
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-19 17:11:57 +0545

    wallpaper archive

--- home-manager/modules/wallpaper-archive.nix
+++ home-manager/modules/wallpaper-archive.nix
@@ -1,15 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   wallpapers = pkgs.fetchFromGitHub {
     owner = "santoshxshrestha";
     repo = "wallpaper-archive";
-    rev = "commit hash";
-    hash = "";
+    rev = "main";
+    sha256 = lib.fakeSha256;
   };
 in {
   home.file = {
     "Pictures/wallpaper-archive" = {
       source = "${wallpapers}/wallpaper-archive";
+      recursive = true;
     };
   };
   # stylix.image = "${wallpapers}/path/to/wallpaper.png";

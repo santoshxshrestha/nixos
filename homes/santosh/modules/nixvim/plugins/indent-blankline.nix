3e98e0c80c65cad019bd65fb8f7c630eba36338a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-24 02:06:04 +0545

    update the config of the blank line

--- homes/santosh/modules/nixvim/plugins/indent-blankline.nix
+++ homes/santosh/modules/nixvim/plugins/indent-blankline.nix
@@ -1,20 +1,12 @@
 {
   programs.nixvim.plugins.indent-blankline = {
-    enable = false;
+    enable = true;
     settings = {
-      indent = {
-        char = "│";
-        tab_char = "│"; # Character for tab indentation
-      };
-      whitespace = {
-        highlight =
-          [ "Whitespace" "NonText" ]; # Highlight groups for whitespace
-      };
+      indent = { char = "│"; };
       scope = {
         show_start = false;
         show_end = false;
         show_exact_scope = false;
-        highlight = [ "Function" "Label" ]; # Highlight groups for scope
       };
     };
   };

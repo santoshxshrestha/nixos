796944a5f66506e78498bd35573743b38fd862b8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-23 16:29:12 +0545

    did base settings

--- homes/santosh/modules/waybar.nix
+++ homes/santosh/modules/waybar.nix
@@ -1,1 +1,54 @@
-{ ... }: { waybar = { enable = true; }; }
+{ config, pkgs, ... }: {
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layers = "top";
+        position = "top";
+        height = 24;
+        spacing = 2;
+        margin-top = 4;
+        margin-left = 8;
+        margin-right = 8;
+
+        modules-left = [ "hyprland/workspaces" "hyprland/window" ];
+        modules-center = [ "clock" ];
+        modules-right = [
+          "network"
+          "backlight"
+          "pulseaudio"
+          "pulseaudio#microphone"
+          "battery"
+          "tray"
+          "custom/notification"
+        ];
+
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          format = "{name}";
+          format-icons = {
+            "1" = "1";
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            "5" = "5";
+            "6" = "6";
+            "7" = "7";
+            "8" = "8";
+            "9" = "9";
+            "10" = "10";
+          };
+          persistent_workspaces = {
+            "1" = [ ];
+            "2" = [ ];
+            "3" = [ ];
+            "4" = [ ];
+            "5" = [ ];
+          };
+        };
+
+      };
+    };
+  };
+}

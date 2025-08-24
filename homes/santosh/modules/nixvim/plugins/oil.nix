d7732a8019c09168b2f3fd59df3da88f85029d74
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-25 00:26:21 +0545

    update the config of the oilnvim

--- homes/santosh/modules/nixvim/plugins/oil.nix
+++ homes/santosh/modules/nixvim/plugins/oil.nix
@@ -2,6 +2,7 @@
   programs.nixvim.plugins.oil = {
     enable = true;
     settings = {
+      win_options = { wrap = true; };
       skip_confirm_for_simple_edits = true;
       use_default_keymaps = true;
       view_options = {

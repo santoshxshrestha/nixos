6ca2cff9939c3dbfba6c2d8c29183e2925355af9
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-26 23:17:47 +0545

    I will be using the netrw as main Ex and oil as backup

--- homes/santosh/modules/nixvim/plugins/oil.nix
+++ homes/santosh/modules/nixvim/plugins/oil.nix
@@ -2,6 +2,7 @@
   programs.nixvim.plugins.oil = {
     enable = true;
     settings = {
+      default_file_explorer = false;
       win_options = { wrap = true; };
       skip_confirm_for_simple_edits = true;
       use_default_keymaps = true;

eaaba8c951b5f3f57084a3b9523aa87ce69c413b
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-27 18:50:59 +0545

    update the config of the hyprpaper

--- homes/santosh/modules/hyprpaper.nix
+++ homes/santosh/modules/hyprpaper.nix
@@ -1,11 +1,19 @@
-{
+{ config, ... }: {
   services.hyprpaper = {
     enable = true;
     settings = {
       splash = true;
       splash_offset = 1.2;
-      splash_color = "55ffffff";
       ipc = true;
+      preload = [
+        # "${config.home.homeDirectory}/nixos/wallpapers/anime-night-sky-illustration.jpg"
+        # "${config.home.homeDirectory}/nixos/wallpapers/nix-dark.png"
+      ];
+
+      wallpaper = [
+        # "DP-3,/share/wallpapers/buttons.png"
+        # "eDP-1,${config.home.homeDirectory}/nixos/wallpapers/anime-night-sky-illustration.jpg"
+      ];
     };
   };
 }

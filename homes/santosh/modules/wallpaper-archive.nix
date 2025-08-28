0d937cb322de563789e3e8daf0e134bdcddc2935
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-28 16:32:59 +0545

    hash has been set and module is injected to the home.nix

--- homes/santosh/modules/wallpaper-archive.nix
+++ homes/santosh/modules/wallpaper-archive.nix
@@ -4,12 +4,13 @@ let
     owner = "santoshxshrestha";
     repo = "wallpaper-archive";
     rev = "main";
-    sha256 = lib.fakeSha256;
+    # sha256 = lib.fakeSha256;
+    sha256 = "sha256-MwWc27LiZcg5oi+brOmZV4JkWrAezRuN+Z2hQBh7q3I=";
   };
 in {
   home.file = {
     "Pictures/wallpaper-archive" = {
-      source = "${wallpapers}/wallpaper-archive";
+      source = "${wallpapers}";
       recursive = true;
     };
   };

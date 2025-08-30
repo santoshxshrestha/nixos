964cac2e0b7c3842221766f923bd683936db42bc
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-31 00:46:36 +0545

    update the fish config

--- homes/santosh/modules/fish.nix
+++ homes/santosh/modules/fish.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, ... }:
-
-{
+{ config, ... }: {
   programs.fish = {
     enable = true;
+    generateCompletions = true;
+    interactiveShellInit = "fastfetch";
 
     shellInit = ''
       set -gx EDITOR nvim
@@ -18,26 +18,29 @@
 
       set -U fish_greeting
 
-      set -gx PATH ~/bin $PATH
-      set -gx PATH ~/.local/scripts $PATH
-      set -gx PATH ~/.cargo/bin $PATH
+      set -gx PATH ${config.home.homeDirectory}/bin $PATH
+      set -gx PATH ${config.home.homeDirectory}/.local/scripts $PATH
+      set -gx PATH ${config.home.homeDirectory}/.cargo/bin $PATH
     '';
-
     shellAbbrs = {
+      "initialize" = "flake-initializer";
+      "b" = "beam";
+    };
+
+    shellAliases = {
       ".." = "cd ..";
       "ls" = "ls --color";
 
-      "asdf" = "~/.local/scripts/sessionizer";
-      "adsf" = "~/.local/scripts/sessionizer";
-      "adfs" = "~/.local/scripts/sessionizer";
+      "asdf" = "${config.home.homeDirectory}/.local/scripts/sessionizer";
+      "adsf" = "${config.home.homeDirectory}/.local/scripts/sessionizer";
+      "adfs" = "${config.home.homeDirectory}/.local/scripts/sessionizer";
 
-      "rebuild" = "sudo nixos-rebuild switch --flake ~/nixos/#santosh";
-      "initialize" = "flake-initializer";
+      "rebuild" =
+        "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/nixos/#santosh";
 
       "gs" = "git status";
       "gd" = "git diff";
       "ga" = "git add .";
-      "b" = "beam";
       "c" = "git-commit";
       "P" = "git push origin $(git branch --show-current)";
     };

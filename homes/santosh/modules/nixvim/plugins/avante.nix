cdea2bbddb454e251e276689a4d89edfb37369a8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-20 19:41:41 +0545

    nixvim : make togglable

--- homes/santosh/modules/nixvim/plugins/avante.nix
+++ homes/santosh/modules/nixvim/plugins/avante.nix
@@ -1,12 +1,21 @@
-{
-  programs.nixvim.plugins.avante = {
-    enable = true;
-    settings = {
-      provider = "copilot";
-      model = "gpt-4";
-      window = {
-        border = "rounded";
-        wrapping = true;
+{ config, lib, ... }: {
+  options = {
+    avante.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable avante.nvim (AI assistant for nvim)";
+    };
+  };
+  config = lib.mkIf config.avante.enable {
+    programs.nixvim.plugins.avante = {
+      enable = true;
+      settings = {
+        provider = "copilot";
+        model = "gpt-4";
+        window = {
+          border = "rounded";
+          wrapping = true;
+        };
       };
     };
   };

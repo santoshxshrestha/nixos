f0db447e30d53f01767ce6fb337052c68d3cf8d8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-20 19:54:38 +0545

    nixvim: added copilot-chat plugin togglable

--- homes/santosh/modules/nixvim/plugins/copilot-chat.nix
+++ homes/santosh/modules/nixvim/plugins/copilot-chat.nix
@@ -1,16 +1,25 @@
-{
-  programs.nixvim.plugins.copilot-chat = {
-    enable = true;
-    settings = {
-      model = "gpt-4o";
-      window = {
-        height = 0.9;
-        width = 0.9;
-        border = "rounded";
-        # title = "[ ]";
-        title = "   AutoPilot ";
-        # replace is also a good option
-        layout = "float";
+{ config, lib, ... }: {
+  options = {
+    copilot-chat.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable copilot-chat.nvim (ChatGPT integration for nvim)";
+    };
+  };
+  config = lib.mkIf config.copilot-chat.enable {
+    programs.nixvim.plugins.copilot-chat = {
+      enable = true;
+      settings = {
+        model = "gpt-4o";
+        window = {
+          height = 0.9;
+          width = 0.9;
+          border = "rounded";
+          # title = "[ ]";
+          title = "   AutoPilot ";
+          # replace is also a good option
+          layout = "float";
+        };
       };
     };
   };

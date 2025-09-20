2d9bdd08c007e538048403407868984ecc5469c8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-20 19:42:35 +0545

    nixvim: made colorscheme toggleable

--- homes/santosh/modules/nixvim/plugins/colorschemes.nix
+++ homes/santosh/modules/nixvim/plugins/colorschemes.nix
@@ -1,129 +1,140 @@
-{ pkgs, ... }: {
-  programs.nixvim = {
+{ pkgs, config, lib, ... }: {
+  options = {
     colorschemes = {
-
-      catppuccin = {
-        enable = true;
-        settings = {
-          show_end_of_buffer = false;
-          transparent_background = false;
-          flavor = "mocha";
-          float = {
-            transparent = true;
-            solid = false;
-          };
-          color_overrides = { all = { base = "#191d33"; }; };
-          no_italic = false;
-          no_bold = true;
-          no_underline = false;
-        };
-      };
-
-      kanagawa = {
-        enable = false;
-        settings = {
-          transparent = false;
-          commonStyle = {
-            italic = true;
-            bold = false;
-          };
-          undercurl = true;
-        };
-      };
-
-      palette = {
-        enable = false;
-        settings = {
-          italics = true;
-          bolds = false;
-          transparent_background = true;
-        };
-      };
-
-      rose-pine = {
-        enable = false;
-        settings = {
-          show_end_of_buffer = false;
-          flavor = "main";
-          disable_background = true;
-          disable_float_background = true;
-          styles = {
-            bold = false;
-            italic = true;
-            transparency = true;
-          };
-        };
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable various colorschemes for nixvim";
       };
     };
+  };
+  config = lib.mkIf config.colorschemes.enable {
+    programs.nixvim = {
+      colorschemes = {
 
-    extraPlugins = [
-      (pkgs.vimPlugins.falcon)
-
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "vague-nvim";
-        version = "1.4.1";
-        src = pkgs.fetchFromGitHub {
-          owner = "vague2k";
-          repo = "vague.nvim";
-          rev = "v1.4.1";
-          sha256 = "sha256-isROQFePz8ofJg0qa3Avbwh4Ml4p9Ii2d+VAAkbeGO8=";
+        catppuccin = {
+          enable = true;
+          settings = {
+            show_end_of_buffer = false;
+            transparent_background = false;
+            flavor = "mocha";
+            float = {
+              transparent = true;
+              solid = false;
+            };
+            color_overrides = { all = { base = "#191d33"; }; };
+            no_italic = false;
+            no_bold = true;
+            no_underline = false;
+          };
         };
-      })
 
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "black-metal-theme-neovim";
-        version = "6d0207871387077f40d5396ab1ae90520e688d36";
-        src = pkgs.fetchFromGitHub {
-          owner = "metalelf0";
-          repo = "black-metal-theme-neovim";
-          rev = "6d0207871387077f40d5396ab1ae90520e688d36";
-          sha256 = "sha256-sRbXxekmQuL412AJKrSkI1EdcuYQkKm1qfcIyMNhLBA=";
+        kanagawa = {
+          enable = false;
+          settings = {
+            transparent = false;
+            commonStyle = {
+              italic = true;
+              bold = false;
+            };
+            undercurl = true;
+          };
         };
-      })
 
-    ];
+        palette = {
+          enable = false;
+          settings = {
+            italics = true;
+            bolds = false;
+            transparent_background = true;
+          };
+        };
 
-    extraConfigLua = ''
-      require('vague').setup({
-          transparent = false,
-          bold = false,
-          italic = true,
-          -- colors = {
-          --     bg = "#191d33",
-          -- },
-          on_highlights = function(highlights, colors)
-              -- GitSigns
-              highlights.GitSignsAdd = { fg = "#ffffff", bg = "NONE" }
-              highlights.GitSignsChange = { fg = "#aaaaaa", bg = "NONE" }
-              highlights.GitSignsDelete = { fg = colors.error, bg = "NONE" }
+        rose-pine = {
+          enable = false;
+          settings = {
+            show_end_of_buffer = false;
+            flavor = "main";
+            disable_background = true;
+            disable_float_background = true;
+            styles = {
+              bold = false;
+              italic = true;
+              transparency = true;
+            };
+          };
+        };
+      };
 
-              -- IndentBlankline
-              highlights.IndentBlanklineChar = { fg = "#3b3b3b", nocombine = true }
-              highlights.IndentBlanklineContextChar = { fg = colors.plus, nocombine = true }
-              highlights.IndentBlanklineSpaceChar = { fg = "#444444", nocombine = true }
+      extraPlugins = [
+        (pkgs.vimPlugins.falcon)
 
-          end,
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "vague-nvim";
+          version = "1.4.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "vague2k";
+            repo = "vague.nvim";
+            rev = "v1.4.1";
+            sha256 = "sha256-isROQFePz8ofJg0qa3Avbwh4Ml4p9Ii2d+VAAkbeGO8=";
+          };
+        })
 
-          -- style = {
-          --   boolean = "italic",
-          --   number = "none",
-          --   float = "none",
-          --   error = "none",
-          --   comments = "italic",
-          --   conditionals = "none",
-          --   functions = "none",
-          --   headings = "bold",
-          --   operators = "none",
-          --   strings = "none",
-          --   variables = "none",
-          --   keywords = "none",
-          --   keyword_return = "none",
-          --   keywords_loop = "none",
-          --   keywords_label = "none",
-          --   keywords_exception = "none",
-          -- },
-      })
-       -- vim.cmd('colorscheme vague')
-    '';
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "black-metal-theme-neovim";
+          version = "6d0207871387077f40d5396ab1ae90520e688d36";
+          src = pkgs.fetchFromGitHub {
+            owner = "metalelf0";
+            repo = "black-metal-theme-neovim";
+            rev = "6d0207871387077f40d5396ab1ae90520e688d36";
+            sha256 = "sha256-sRbXxekmQuL412AJKrSkI1EdcuYQkKm1qfcIyMNhLBA=";
+          };
+        })
+
+      ];
+
+      extraConfigLua = ''
+        require('vague').setup({
+            transparent = false,
+            bold = false,
+            italic = true,
+            -- colors = {
+            --     bg = "#191d33",
+            -- },
+            on_highlights = function(highlights, colors)
+                -- GitSigns
+                highlights.GitSignsAdd = { fg = "#ffffff", bg = "NONE" }
+                highlights.GitSignsChange = { fg = "#aaaaaa", bg = "NONE" }
+                highlights.GitSignsDelete = { fg = colors.error, bg = "NONE" }
+
+                -- IndentBlankline
+                highlights.IndentBlanklineChar = { fg = "#3b3b3b", nocombine = true }
+                highlights.IndentBlanklineContextChar = { fg = colors.plus, nocombine = true }
+                highlights.IndentBlanklineSpaceChar = { fg = "#444444", nocombine = true }
+
+            end,
+
+            -- style = {
+            --   boolean = "italic",
+            --   number = "none",
+            --   float = "none",
+            --   error = "none",
+            --   comments = "italic",
+            --   conditionals = "none",
+            --   functions = "none",
+            --   headings = "bold",
+            --   operators = "none",
+            --   strings = "none",
+            --   variables = "none",
+            --   keywords = "none",
+            --   keyword_return = "none",
+            --   keywords_loop = "none",
+            --   keywords_label = "none",
+            --   keywords_exception = "none",
+            -- },
+        })
+         -- vim.cmd('colorscheme vague')
+      '';
+    };
   };
 }

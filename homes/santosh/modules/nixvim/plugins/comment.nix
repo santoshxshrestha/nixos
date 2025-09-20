598b5af183aa1b0be5cc1f92a637ecc4e0c6f09a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-20 19:43:33 +0545

    nixvim: made comment plugin togglable

--- homes/santosh/modules/nixvim/plugins/comment.nix
+++ homes/santosh/modules/nixvim/plugins/comment.nix
@@ -1,17 +1,27 @@
-{
-  programs.nixvim.plugins.comment = {
-    enable = true;
-    settings = {
-      # Comment.nvim default settings
-      padding = true;
-      sticky = true;
-      ignore = null;
-      mappings = {
-        basic = false;
-        extra = false;
+{ config, lib, ... }: {
+  options = {
+    comment.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description =
+        "Enable comment.nvim (Smart and powerful comment plugin for neovim)";
+    };
+  };
+  config = lib.mkIf config.comment.enable {
+    programs.nixvim.plugins.comment = {
+      enable = true;
+      settings = {
+        # Comment.nvim default settings
+        padding = true;
+        sticky = true;
+        ignore = null;
+        mappings = {
+          basic = false;
+          extra = false;
+        };
+        pre_hook = null;
+        post_hook = null;
       };
-      pre_hook = null;
-      post_hook = null;
     };
   };
 }

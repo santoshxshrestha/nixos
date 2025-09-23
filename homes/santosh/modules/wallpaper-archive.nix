5dc192f60d62835fd11cb3269bbbdd66c60dd899
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-24 00:22:45 +0545

    wallpaper archive: update to latest version

--- homes/santosh/modules/wallpaper-archive.nix
+++ homes/santosh/modules/wallpaper-archive.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   wallpapers = pkgs.fetchFromGitHub {
     owner = "santoshxshrestha";
     repo = "wallpaper-archive";
     rev = "main";
-    sha256 = "sha256-YGuw+ADfTIeguvdi5rjJhQrtpOdEEQ/hG2wID4ilFfc=";
+    sha256 = "sha256-0OmIh1WFPMbvsioTmhGjCTNBuFrPoLgMvIN49vhm7d8=";
   };
 in { home.sessionVariables = { WALLPAPER_ARCHIVE_PATH = "${wallpapers}"; }; }

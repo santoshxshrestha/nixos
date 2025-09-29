040935e0300147475150bdc8eb75052db10fdbf0
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-29 22:37:04 +0545

    nixvim : transparent is all i want

--- homes/santosh/modules/nixvim/plugins/colorschemes.nix
+++ homes/santosh/modules/nixvim/plugins/colorschemes.nix
@@ -22,7 +22,7 @@
           enable = true;
           settings = {
             show_end_of_buffer = false;
-            transparent_background = false;
+            transparent_background = true;
             flavor = "mocha";
             float = {
               transparent = true;

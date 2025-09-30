7a09170e180cacf687d719aabc3d2599c617f0cf
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-01 01:48:29 +0545

    script: function is better

--- homes/santosh/modules/scripts/music-player.nix
+++ homes/santosh/modules/scripts/music-player.nix
@@ -2,23 +2,44 @@
 let
   play = pkgs.writeShellScriptBin "play" ''
     if [[ -z "$1" ]]; then
-      echo "Usage: play <url|haven|lofi>"
-      exit 1
+    echo "Usage: play [--select] <url|haven|lofi>"
+    exit 1
+    fi
+
+    select_mode=false
+    if [[ "$1" == "--select" ]]; then
+    select_mode=true
+    shift
     fi
 
     if [[ "$1" == "haven" ]]; then
-      url="https://youtube.com/playlist?list=PLBBcaljFGSiSz8czPRv3tX3C4nKo4On4j&si=pXh1Ru-7vK-3evs5"
-      selected=$("${pkgs.yt-dlp}/bin/yt-dlp" --flat-playlist -J "$url" | "${pkgs.jq}/bin/jq" -r '.entries[] | .title + " " + .url' | "${pkgs.fzf}/bin/fzf" | awk '{print $NF}')
-      if [[ -n "$selected" ]]; then
-        "${pkgs.mpv}/bin/mpv" --no-video "$selected"
-      else
-        echo "No selection made."
-      fi
-    elif [[ "$1" == "lofi" ]]; then
-    url="https://youtube.com/playlist?list=PLBBcaljFGSiSMYqaq-Vs-1hmnMyCvzW7D&si=Lcd-U9CV3pMydS6k"
-      "${pkgs.mpv}/bin/mpv" --no-video --shuffle --loop-playlist=inf "$url"
+    url="https://youtube.com/playlist?list=PLBBcaljFGSiSz8czPRv3tX3C4nKo4On4j&si=pXh1Ru-7vK-3evs5"
+    if $select_mode; then
+    selected=$("${pkgs.yt-dlp}/bin/yt-dlp" --flat-playlist -J "$url" | "${pkgs.jq}/bin/jq" -r '.entries[] | .title + " " + .url' | "${pkgs.fzf}/bin/fzf" | awk '{print $NF}')
+    if [[ -n "$selected" ]]; then
+    "${pkgs.mpv}/bin/mpv" --no-video "$selected"
     else
-      "${pkgs.mpv}/bin/mpv" --no-video "$1"
+    echo "No selection made."
+    fi
+    fi
+    fi
+
+    if [[ "$1" == "lofi" ]]; then
+    url="https://youtube.com/playlist?list=PLBBcaljFGSiSMYqaq-Vs-1hmnMyCvzW7D&si=Lcd-U9CV3pMydS6k"
+    if $select_mode; then
+    selected=$("${pkgs.yt-dlp}/bin/yt-dlp" --flat-playlist -J "$url" | "${pkgs.jq}/bin/jq" -r '.entries[] | .title + " " + .url' | "${pkgs.fzf}/bin/fzf" | awk '{print $NF}')
+    if [[ -n "$selected" ]]; then
+    "${pkgs.mpv}/bin/mpv" --no-video "$selected"
+    else
+    echo "No selection made."
+    fi
+    else
+    "${pkgs.mpv}/bin/mpv" --no-video --shuffle --loop-playlist=inf "$url"
+    fi
+    fi
+
+    if [[ "$1" != "haven" && "$1" != "lofi" ]]; then
+    "${pkgs.mpv}/bin/mpv" --no-video "$1"
     fi
   '';
 in

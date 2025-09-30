4aba60a16ab0412d3d0827adc17f4bff014c1b09
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-01 01:02:29 +0545

    script: update script to be shuffling songs

--- homes/santosh/modules/scripts/music-player.nix
+++ homes/santosh/modules/scripts/music-player.nix
@@ -7,7 +7,9 @@ let
     fi
 
     if [[ "$1" == "haven" ]]; then
-      "${pkgs.mpv}/bin/mpv" --no-video "https://youtube.com/playlist?list=PLBBcaljFGSiSz8czPRv3tX3C4nKo4On4j&si=pXh1Ru-7vK-3evs5"
+      "${pkgs.mpv}/bin/mpv" --no-video --shuffle --loop-playlist=inf "https://youtube.com/playlist?list=PLBBcaljFGSiSz8czPRv3tX3C4nKo4On4j&si=pXh1Ru-7vK-3evs5"
+    elif [[ "$1" == "lofi" ]]; then
+      "${pkgs.mpv}/bin/mpv" --no-video --shuffle --loop-playlist=inf "https://youtube.com/playlist?list=PLBBcaljFGSiSMYqaq-Vs-1hmnMyCvzW7D&si=Lcd-U9CV3pMydS6k"
     else
       "${pkgs.mpv}/bin/mpv" --no-video "$1"
     fi

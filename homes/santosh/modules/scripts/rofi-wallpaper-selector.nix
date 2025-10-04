ac10cb9aa48c3e43a198422a849ade62ca79eaf8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-05 00:59:30 +0545

    wallpaper: added rofi config for the wallpaper selection

--- homes/santosh/modules/scripts/rofi-wallpaper-selector.nix
+++ homes/santosh/modules/scripts/rofi-wallpaper-selector.nix
@@ -13,19 +13,19 @@ let
     hyprpaper &
     sleep 1
     fi
-    WALLPAPER=$(find "$WALLPAPER_DIR" | fzf)
+    # WALLPAPER=$(find "$WALLPAPER_DIR" | fzf)
 
-    # WALLPAPER=$(kitty -e bash -c "fzf $(find "$WALLPAPER_DIR")")
-    # WALLPAPER=$(kitty -e bash -c "fzf $(find "$WALLPAPER_DIR") || exit")
-    # WALLPAPER=$(kitty -e bash -c "$WALLPAPER_DIR" | fzf)
-    # WALLPAPER=
-    # for a in "$WALLPAPER_DIR"/*; do
-    #     echo -en "$a\0icon\x1f$a\n"
-    # done | rofi -dmenu
+    WALLPAPER=$( for a in "$WALLPAPER_DIR"/*; do
+    echo -en "$a\0icon\x1f$a\n"
+    done | rofi -dmenu -p "wallpaper selector" -theme ~/.config/rofi/themes/wallpaper-selector.rasi)
 
     # WALLPAPER=$(find "$WALLPAPER_DIR" | rofi -dmenu -p "wallpaper selector" -theme ~/.config/rofi/themes/clipboard.rasi)
+    if [ -z "$WALLPAPER" ]; then
+    notify-send "No wallpaper selected, exiting..."
+    fi
 
     hyprctl hyprpaper reload ,"$WALLPAPER"
+    notify-send "Wallpaper changed to $(basename "$WALLPAPER")"
   '';
 in
 {

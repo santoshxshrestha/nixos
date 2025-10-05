c1082507293301dfad35978196d0f6ae9711489a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-05 18:35:05 +0545

    swaync: config update for the nc

--- homes/santosh/modules/swaync.nix
+++ homes/santosh/modules/swaync.nix
@@ -8,10 +8,10 @@
       control-center-layer = "top";
       layer-shell = true;
       cssPriority = "user";
-      control-center-margin-top = 10;
-      control-center-margin-bottom = 10;
+      control-center-margin-top = 20;
+      control-center-margin-bottom = 60;
       control-center-margin-right = 10;
-      control-center-margin-left = 10;
+      control-center-margin-left = 20;
       notification-2fa-action = true;
       notification-inline-replies = true;
       notification-icon-size = 72;

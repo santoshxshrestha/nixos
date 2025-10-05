df3aa4b05e455f57b5d70f1a14f2eb84cc286702
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-05 13:22:50 +0545

    wallpaper-archive: update to latest version

--- homes/santosh/modules/wallpaper-archive.nix
+++ homes/santosh/modules/wallpaper-archive.nix
@@ -4,7 +4,7 @@ let
     owner = "santoshxshrestha";
     repo = "wallpaper-archive";
     rev = "main";
-    hash = "sha256-E9zMnNwrsY/UlJMNS6788USeWiv0bDlCB/3EwgTuYUE=";
+    hash = "sha256-jHft+OUMevKUj0GNmd+B3nDPaqj+94KsnjS9pW7PBjA=";
   };
 in
 {

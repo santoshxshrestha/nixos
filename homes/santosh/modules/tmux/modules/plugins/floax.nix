af8619f9fe9ac87d8c31e68fd99c9acee6085a6d
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-24 22:51:50 +0545

    changed the height and width of the panel

--- homes/santosh/modules/tmux/modules/plugins/floax.nix
+++ homes/santosh/modules/tmux/modules/plugins/floax.nix
@@ -9,7 +9,7 @@
     # M- means "hold Meta/Alt"
     set -g @floax-bind '-n M-o'
     set -g @floax-title  '   #{user}@#{host} '
-    set -g @floax-width '90%'
-    set -g @floax-height '90%'
+    set -g @floax-width '100%'
+    set -g @floax-height '100%'
   '';
 }

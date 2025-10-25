2aa34652827d7da26080aa016ed8495eafc10251
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-25 22:23:36 +0545

    sys: update the latest version of the homemanager and nixvim (treesitter-plug) config changed according to new version

--- homes/santosh/modules/nixvim/modules/plugins/treesitter.nix
+++ homes/santosh/modules/nixvim/modules/plugins/treesitter.nix
@@ -31,36 +31,38 @@
 
     programs.nixvim.plugins.treesitter-textobjects = {
       enable = true;
-      select = {
-        enable = true;
-        lookahead = true;
+      settings = {
+        select = {
+          enable = true;
+          lookahead = true;
 
-        keymaps = {
-          "af" = "@function.outer";
-          "if" = "@function.inner";
-          "ac" = "@class.outer";
-          "ic" = {
-            query = "@class.inner";
-            desc = "Select inner class";
+          keymaps = {
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "ac" = "@class.outer";
+            "ic" = {
+              query = "@class.inner";
+              desc = "Select inner class";
+            };
+            "as" = {
+              query = "@local.scope";
+              queryGroup = "locals";
+              desc = "Select language scope";
+            };
+            "aa" = "@parameter.outer";
+            "ia" = "@parameter.inner";
+            "il" = "@loop.inner";
+            "al" = "@loop.outer";
           };
-          "as" = {
-            query = "@local.scope";
-            queryGroup = "locals";
-            desc = "Select language scope";
+
+          selectionModes = {
+            "@parameter.outer" = "v";
+            "@function.outer" = "V";
+            "@class.outer" = "<c-v>";
           };
-          "aa" = "@parameter.outer";
-          "ia" = "@parameter.inner";
-          "il" = "@loop.inner";
-          "al" = "@loop.outer";
-        };
 
-        selectionModes = {
-          "@parameter.outer" = "v";
-          "@function.outer" = "V";
-          "@class.outer" = "<c-v>";
+          includeSurroundingWhitespace = true;
         };
-
-        includeSurroundingWhitespace = true;
       };
     };
   };

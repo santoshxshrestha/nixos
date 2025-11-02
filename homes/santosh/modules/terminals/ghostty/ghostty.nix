a6a5e7b25e455d4360bffc9fa56dd77b961ab685
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-02 18:22:39 +0545

    terminal: disable the cursor anim

--- homes/santosh/modules/terminals/ghostty/ghostty.nix
+++ homes/santosh/modules/terminals/ghostty/ghostty.nix
@@ -11,9 +11,12 @@
       font-family-italic = "JetBrainsMono Nerd Font";
       font-family-bold-italic = "JetBrainsMono Nerd Font";
       custom-shader-animation = true;
-      custom-shader = "${config.home.homeDirectory}/nixos/homes/santosh/modules/terminals/ghostty/shaders/coil.glsl";
-      # custom-shader = "${config.home.homeDirectory}/nixos/homes/santosh/modules/terminals/ghostty/shaders/glow.glsl";
+      # custom-shader = "${config.home.homeDirectory}/nixos/homes/santosh/modules/terminals/ghostty/shaders/coil.glsl";
+      custom-shader = "${config.home.homeDirectory}/nixos/homes/santosh/modules/terminals/ghostty/shaders/glow.glsl";
       mouse-hide-while-typing = true;
+      cursor-style = "block";
+      # cursor-style-blink = true;
+      # adjust-cursor-height = "-25%";
     };
     themes = {
       dark-funeral = {

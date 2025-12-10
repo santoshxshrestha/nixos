9fc7d5cb9ac3dedf9b72099b2f0e4ab945a285cf
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-10 23:39:52 +0545

    pkgs: added wlsunset module

---
+++ homes/santosh/modules/wlsunset.nix
@@ -0,0 +1,16 @@
+{
+  services.wlsunset = {
+    enable = true;
+    sunrise = "06:00";
+    sunset = "18:00";
+    latitude = 27;
+    longitude = 83;
+    gamma = 1.5;
+    temperatur = {
+      day = 6000;
+      night = 3000;
+
+    };
+
+  };
+}

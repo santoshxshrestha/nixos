332ff0c2b9d4103dd678652ab30b6d9e1deefb6c
santoshxshrestha <santoshxshrestha@gmail.com>
2026-01-16 20:23:38 +0545

    home: cursor changed to babita

--- homes/santosh/home.nix
+++ homes/santosh/home.nix
@@ -158,21 +158,21 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # home.pointerCursor = {
-  #   gtk.enable = true;
-  #   x11.enable = true;
-  #   package = pkgs.bibata-cursors;
-  #   name = "Bibata-Original-Ice";
-  #   size = 20;
-  # };
-
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
-    package = pkgs.nightdiamond-cursors;
-    name = "NightDiamond-Blue";
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Original-Ice";
     size = 20;
   };
+
+  # home.pointerCursor = {
+  #   gtk.enable = true;
+  #   x11.enable = true;
+  #   package = pkgs.nightdiamond-cursors;
+  #   name = "NightDiamond-Blue";
+  #   size = 20;
+  # };
 
   programs.direnv = {
     enable = true;

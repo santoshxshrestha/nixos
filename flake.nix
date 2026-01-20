4ad51bf1e0bf9730cc78c665dc83eb67743f1e6c
santoshxshrestha <santoshxshrestha@gmail.com>
2026-01-20 23:02:46 +0545

    home: (formatter) fix the latest formater adoption

--- flake.nix
+++ flake.nix
@@ -69,6 +69,6 @@
         };
 
       };
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt;
     };
 }

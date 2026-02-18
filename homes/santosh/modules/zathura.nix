20929336d8c5d1ac50c56bdc90f69e0ee48e33c2
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-18 23:42:12 +0545

    zathura: update the fg and bg color of zathura

--- homes/santosh/modules/zathura.nix
+++ homes/santosh/modules/zathura.nix
@@ -2,10 +2,14 @@
   programs.zathura = {
     enable = true;
     options = {
-      default-bg = "#1a1b26";
+      # default-bg = "#1a1b26";
+      default-bg = "rgba(0,0,0,0.8)";
       default-fg = "#c0caf5";
 
       recolor = true;
+
+      # recolor-darkcolor = "#FFFFFF";
+      recolor-lightcolor = "rgba(0,0,0,0)";
 
       # copy selections to the system cliboard
       selection-clipboard = "clipboard";

08dc189c9da597935739aac6e3d8cc2dd815bb30
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-19 14:58:59 +0545

    zathura: ricing this will go easy on my eyes

--- homes/santosh/modules/zathura.nix
+++ homes/santosh/modules/zathura.nix
@@ -4,16 +4,18 @@
     options = {
       # default-bg = "#1a1b26";
       default-bg = "rgba(0,0,0,0.8)";
-      default-fg = "#c0caf5";
+
+      # default-fg = "#C5C8C6";
+      default-fg = "#C6C1B4";
 
       recolor = true;
 
       # text should be some what easy on eyes
-      recolor-darkcolor = "#c0caf5";
+      # recolor-darkcolor = "#C5C8C6";
+      recolor-darkcolor = "#C6C1B4";
 
       # transparent color rather then white backround color
       recolor-lightcolor = "rgba(0,0,0,0)";
-
       # copy selections to the system cliboard
       selection-clipboard = "clipboard";
     };

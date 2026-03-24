bc391495ccc720fc9e4124db13ed9f2331e69389
vimlinuz <vimlinuz@gmail.com>
2026-03-25 01:44:54 +0545

    gtk: update the option according to latest options

--- homes/santosh/modules/gtk.nix
+++ homes/santosh/modules/gtk.nix
@@ -2,17 +2,30 @@
 {
   gtk = {
     enable = true;
+
+    colorScheme = "dark";
+
     theme = {
       name = "Tokyonight-Dark";
       package = pkgs.tokyonight-gtk-theme;
     };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+
+    gtk2 = {
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+
+      font = {
+        name = "JetBrainsMono Nerd Font";
+        size = 8;
+      };
+
     };
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 8;
+
+    gtk4 = {
+      theme = null;
     };
+
   };
 }

86fcac2b00f40e4ddb3172c32b63ae37257cbb13
vimlinuz <vimlinuz@gmail.com>
2026-04-03 02:06:42 +0545

    fix: update the colors

--- homes/santosh/modules/tmux/default.nix
+++ homes/santosh/modules/tmux/default.nix
@@ -19,7 +19,8 @@ in
     prefix = "C-a";
     # shell = "${pkgs.fish}/bin/fish";
     shell = "${pkgs.nushell}/bin/nu";
-    terminal = "xterm-256color";
+    terminal = "screen-256color";
+    # terminal = "xterm-256color";
     escapeTime = 0;
     baseIndex = 1;
     plugins = import ./modules/plugins.nix { inherit pkgs colors tmux-session-manager; };

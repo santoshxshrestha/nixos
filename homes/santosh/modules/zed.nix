542a69a86b2b761714be2c026c9fa1d0fcc3697e
vimlinuz <vimlinuz@gmail.com>
2026-04-03 22:01:40 +0545

    editor: udpate config of zed to have vim motion

--- homes/santosh/modules/zed.nix
+++ homes/santosh/modules/zed.nix
@@ -1,5 +1,15 @@
 {
   programs.zed-editor = {
     enable = true;
+    extensions = [
+      "nix"
+      "rust"
+    ];
+    userSettings = {
+      features = {
+        copilot = true;
+      };
+      vim_mode = true;
+    };
   };
 }

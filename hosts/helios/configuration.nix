8df041ce6770fa466c4eaec876b9f0f38b68b0d6
vimlinuz <vimlinuz@gmail.com>
2026-04-07 01:11:27 +0545

    sys: (ports) added 8081 port to allowed ports for expo

--- hosts/helios/configuration.nix
+++ hosts/helios/configuration.nix
@@ -181,7 +181,10 @@
   services.fail2ban.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.allowedTCPPorts = [
+    8080
+    8081
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;

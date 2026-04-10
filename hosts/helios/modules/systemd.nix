bfd90e526f4d175cebcbff937c3fb983b3d3b763
vimlinuz <vimlinuz@gmail.com>
2026-04-10 13:44:10 +0545

    sys: led mod we can do some thign like this too

--- hosts/helios/modules/systemd.nix
+++ hosts/helios/modules/systemd.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   options = {
     systemd.enable = lib.mkEnableOption "Enable systemd settings";
@@ -8,6 +13,23 @@
       sleep.settings.Sleep = {
         HibernateDelaySec = "20min";
       };
+      # services.capslock-blink = {
+      #   wantedBy = [ "multi-user.target" ];
+      #
+      #   serviceConfig = {
+      #     ExecStart = pkgs.writeShellScript "blink" ''
+      #       LED=/sys/class/leds/input11::capslock/brightness
+      #       while true; do
+      #         printf 1 > "$LED"
+      #         sleep 0.2
+      #         printf 0 > "$LED"
+      #         sleep 0.2
+      #       done
+      #     '';
+      #     Restart = "always";
+      #     User = "root";
+      #   };
+      # };
     };
   };
 }

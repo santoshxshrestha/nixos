1bacf97162ae7de74c437a52bcd8e7eb78afebe7
vimlinuz <vimlinuz@gmail.com>
2026-04-11 01:11:05 +0545

    zathura: changed the color to the dark one > [Reason] > the transparent with less alpha was kind of creating flickring while performing scrolling

--- homes/santosh/modules/zathura.nix
+++ homes/santosh/modules/zathura.nix
@@ -2,8 +2,8 @@
   programs.zathura = {
     enable = true;
     options = {
-      # default-bg = "#1a1b26";
-      default-bg = "rgba(0,0,0,0.8)";
+      default-bg = "#1a1b26";
+      # default-bg = "rgba(0,0,0,0.8)";
 
       # default-fg = "#C5C8C6";
       default-fg = "#C6C1B4";
@@ -15,7 +15,8 @@
       recolor-darkcolor = "#C6C1B4";
 
       # transparent color rather then white backround color
-      recolor-lightcolor = "rgba(0,0,0,0)";
+      # recolor-lightcolor = "rgba(0,0,0,0)";
+      recolor-lightcolor = "#1a1b26";
       # copy selections to the system cliboard
       selection-clipboard = "clipboard";
     };

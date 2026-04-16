f5c43626d1c1f07c7804668aa364427ca1813092
vimlinuz <vimlinuz@gmail.com>
2026-04-16 16:38:08 +0545

    sysd: blink little led

--- hosts/helios/modules/systemd.nix
+++ hosts/helios/modules/systemd.nix
@@ -4,6 +4,25 @@
   pkgs,
   ...
 }:
+let
+  blink = pkgs.writeShellScript "blink" ''
+    LED=/sys/class/leds/input10::capslock/brightness
+    echo "blink daemon started at $(date)"
+
+    # Here int might not require but any way
+    trap 'echo "blink daemon stopped"; exit 0' TERM INT
+
+    while true; do
+      printf 1 > "$LED"
+      echo "LED on"
+      sleep 0.2
+
+      printf 0 > "$LED"
+      echo "LED off"
+      sleep 0.2
+    done
+  '';
+in
 {
   options = {
     systemd.enable = lib.mkEnableOption "Enable systemd settings";
@@ -13,23 +32,17 @@
       sleep.settings.Sleep = {
         HibernateDelaySec = "20min";
       };
-      # services.capslock-blink = {
-      #   wantedBy = [ "multi-user.target" ];
-      #
-      #   serviceConfig = {
-      #     ExecStart = pkgs.writeShellScript "blink" ''
-      #       LED=/sys/class/leds/input11::capslock/brightness
-      #       while true; do
-      #         printf 1 > "$LED"
-      #         sleep 0.2
-      #         printf 0 > "$LED"
-      #         sleep 0.2
-      #       done
-      #     '';
-      #     Restart = "always";
-      #     User = "root";
-      #   };
-      # };
+
+      services.capslock-blink = {
+        description = "Blink the capslock LED";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${blink}";
+          Restart = "always";
+          User = "root";
+        };
+      };
+
     };
   };
 }

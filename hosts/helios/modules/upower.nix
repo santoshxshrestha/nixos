ad2c97aff4dceed2cf49a9c5a32199ec2679a3bc
vimlinuz <vimlinuz@gmail.com>
2026-05-02 23:37:45 +0545

    sys: upower-notify added notify

--- hosts/helios/modules/upower.nix
+++ hosts/helios/modules/upower.nix
@@ -1,9 +1,12 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   options = {
     upower.enable = lib.mkEnableOption "Enable upower (power actions service)";
   };
   config = lib.mkIf config.upower.enable {
+    environment.systemPackages = [
+      pkgs.upower-notify
+    ];
     services.upower = {
       enable = true;
       percentageAction = 2;
